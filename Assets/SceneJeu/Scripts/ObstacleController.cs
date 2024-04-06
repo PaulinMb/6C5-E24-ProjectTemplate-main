80589ba7d8654c2a8556138b6dd65c814b36f803
PaulinMb <125313649+PaulinMb@users.noreply.github.com>
2024-04-06 12:12:34 -0400

    particule ok

--- Assets/SceneJeu/Scripts/ObstacleController.cs
+++ Assets/SceneJeu/Scripts/ObstacleController.cs
@@ -18,7 +18,7 @@ public class ObstacleController : MonoBehaviour
     void Update()
     {
         Vector3 position = transform.position;
-        position.z += Mathf.Sin(Time.time * speed) * amplitude;
+        position.x += Mathf.Sin(Time.time * speed) * amplitude;
         //position.x += Mathf.Cos(Time.time * speed) * amplitude;
         transform.position = position;
     }
