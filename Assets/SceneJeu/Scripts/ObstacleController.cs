92749a31362a4e5fbadbc0d14b69eab172405f38
PaulinMb <125313649+PaulinMb@users.noreply.github.com>
2024-04-06 19:34:37 -0400

    limite objet deplacement ok

--- Assets/SceneJeu/Scripts/ObstacleController.cs
+++ Assets/SceneJeu/Scripts/ObstacleController.cs
@@ -4,22 +4,51 @@ using UnityEngine;
 
 public class ObstacleController : MonoBehaviour
 {
+    public GameObject limitObject; // Référence à l'objet qui détermine les limites horizontales
+    public float speed = 2f; // Vitesse du mouvement
 
-    private float amplitude = .1f;
-    private float speed = .1f;
+    private float minX; // Limite horizontale gauche de déplacement
+    private float maxX; // Limite horizontale droite de déplacement
+    private int direction = 1; // Direction du déplacement (1 pour droite, -1 pour gauche)
 
-    // Start is called before the first frame update
     void Start()
     {
+        // Récupérer les limites horizontales de l'objet référencé
+        if (limitObject != null)
+        {
+            Bounds bounds = limitObject.GetComponent<Renderer>().bounds;
+            minX = bounds.min.x;
+            maxX = bounds.max.x;
+        }
+        else
+        {
+            Debug.LogWarning("Limit object reference is missing.");
+            minX = -5f; // Définir une valeur par défaut pour minX
+            maxX = 5f; // Définir une valeur par défaut pour maxX
+        }
 
+        // Initialiser la direction du déplacement aléatoirement
+        direction = Random.Range(0, 2) * 2 - 1; // -1 ou 1
     }
 
-    // Update is called once per frame
     void Update()
     {
-        Vector3 position = transform.position;
-        position.x += Mathf.Sin(Time.time * speed) * amplitude;
-        //position.x += Mathf.Cos(Time.time * speed) * amplitude;
-        transform.position = position;
+        // Calculer le déplacement horizontal en fonction de la vitesse et de la direction
+        float horizontalMovement = direction * speed * Time.deltaTime;
+
+        // Calculer la nouvelle position horizontale
+        float newXPosition = transform.position.x + horizontalMovement;
+
+        // Limiter la nouvelle position horizontale dans les limites récupérées de l'objet référencé
+        newXPosition = Mathf.Clamp(newXPosition, minX, maxX);
+
+        // Mettre à jour la position de l'objet avec la nouvelle position horizontale
+        transform.position = new Vector3(newXPosition, transform.position.y, transform.position.z);
+
+        // Inverser la direction si l'objet atteint l'une des limites horizontales de déplacement
+        if (newXPosition == minX || newXPosition == maxX)
+        {
+            direction *= -1;
+        }
     }
 }
